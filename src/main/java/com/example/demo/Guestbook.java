a252d7ba7750b32fa80a8467ce7cffe83bfd4388
cassie <write_on@hotmail.com>
2020-07-17 16:54:01 -0400

    guestbookfirstcommit

---
+++ src/main/java/com/example/demo/Guestbook.java
@@ -0,0 +1,48 @@
+package com.example.demo;
+
+public class Guestbook {
+
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWishes() {
+        return wishes;
+    }
+
+    public void setWishes(String wishes) {
+        this.wishes = wishes;
+    }
+
+    public String getPostDate() {
+        return postDate;
+    }
+
+    public void setPostDate(String postDate) {
+        this.postDate = postDate;
+    }
+
+    private String name;
+    private String wishes;
+    private String postDate;
+
+    public Guestbook(){
+    }
+
+
+
+}
--- src/main/java/com/example/demo/HomeCtrl.java
+++ src/main/java/com/example/demo/HomeCtrl.java
@@ -2,31 +2,118 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.ArrayList;
 
 @Controller
 public class HomeCtrl {
-    public ArrayList<Employee> allEmployees = new ArrayList<>();
+    public ArrayList<Guestbook> Guestbooks = new ArrayList<>();
 
-    @GetMapping("/employee")
-    public String loadFormPage(Model model){
-        model.addAttribute("employee", new Employee());
-        return "employee";
+    @RequestMapping("/")
+    public String index() {
+        return "index";
     }
-    @PostMapping("/employee")
-    public String loadFromPage(@ModelAttribute Employee employee, Model model) {
-        model.addAttribute("employee, employee");
-        allEmployees.add(employee);
-            return "redirect:/list";
-        }
-    @GetMapping("/list")
-    public String listAll(@ModelAttribute Employee employee, Model model){
-        model.addAttribute("allEmployees", allEmployees);
-        return "list";
 
-
+    @GetMapping("/add")
+    public String addPost(Model model) {
+        model.addAttribute("guestbook", new Guestbook());
+        return "guestbook";
     }
+
 }
+//
+//       @PostMapping("/list")
+//      public String listPost(@ModelAttribute Guestbook guestbook, Model model) {
+//        model.addAttribute("Guestbooks", Guestbooks);
+//        return "list";
+//    }
+//
+//}
+//
+//
+//
+//    public String listGuestbooks(Model model){
+//       model.addAttribute("guestbook", new Guestbook());
+//       return "guestbook";
+//    }
+//
+//    @GetMapping("/add")
+//    public String guestbookForm(Model model) {
+//        model.addAttribute("guestbook", new Guestbook());
+//        model.addAttribute("message", "New Employee");
+//        return "guestbook";
+//    }
+//
+//    @PostMapping("/list")
+//    public String list(@ModelAttribute Guestbook guestbook, Model model){
+//
+//        allGuestbooks.add(guestbook);
+//        return "redirect:/show";
+//    }
+//
+//
+//    @RequestMapping("/show")
+//    public String showAll(Model model){
+//
+//        model.addAttribute("guestbooks", allGuestbooks);
+//        return "list";
+//    }
+//
+//    @RequestMapping("/update/{id}")
+//    public String updateList(@PathVariable("id") long id, Model model){
+//
+//        Guestbook guestbook = new Guestbook();
+//        for (Guestbook one : Guestbooks) {
+//            if (one.getId() == id){
+//                guestbook = one;
+//                Guestbooks.remove(one);
+//                break;
+//            }
+//        }
+//
+//        model.addAttribute("guestbook", guestbook);
+//        model.addAttribute("message", "Update Guestbook");
+//        return "guestbook";
+//    }
+//
+//    @RequestMapping("/delete/{id}")
+//    public String deleteList(@PathVariable("id") long id, Model model){
+//
+//        for (Guestbook guestbook : allGuestbooks) {
+//            if (guestbook.getId() == id){
+//                allGuestbooks.remove(guestbook);
+//                break;
+//            }
+//        }
+//
+//        return "redirect:/show";
+//    }
+//
+//
+//}
+//
+//
+//
+//
+//
+////
+//    @GetMapping("/guestbook")
+//////    public String loadFormPage(Model model){
+//////        model.addAttribute("guestbook", new Guestbook());
+//////        return "guestbook";
+//////    }
+//////    @PostMapping("/guestbook")
+//////    public String loadFromPage(@ModelAttribute Guestbook guestbook, Model model) {
+//////        model.addAttribute("guestbook, guestbook");
+//////        allGuestbooks.add(guestbook);
+//////            return "redirect:/list";
+//////        }
+//////    @GetMapping("/list")
+//////    public String list(@ModelAttribute Guestbook guestbook, Model model){
+//////        model.addAttribute("allGuestbooks", allGuestbooks);
+//////        return "list";
+//////
+//////
+//////    }
+//}
