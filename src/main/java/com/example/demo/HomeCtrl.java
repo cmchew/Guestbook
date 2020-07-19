fa7f4e577f1a12d5d71a2084e4b398b927c816a2
cassie <write_on@hotmail.com>
2020-07-19 02:59:22 -0400

    firstcommit

--- src/main/java/com/example/demo/HomeCtrl.java
+++ src/main/java/com/example/demo/HomeCtrl.java
@@ -10,48 +10,56 @@ import java.util.ArrayList;
 public class HomeCtrl {
     public ArrayList<Post> book = new ArrayList<>();
 
-//View homepage
+    //View homepage
     @RequestMapping("/")
     public String index() {
         return "index";
     }
 
-//Add a new post
+    //Add a new post
     @GetMapping("/newpost")
     public String add(Model model) {
         model.addAttribute("post", new Post());
         return "newpost";
     }
 
-//Get a listing of posts
+    //Get a listing of posts
     @PostMapping("/newpost")
     public String loadFromPage(@ModelAttribute Post post, Model model) {
         model.addAttribute("post", post);
         book.add(post);
         return "redirect:/listpost";
     }
-//Add guest posts to list of posts
+    //Add guest posts to list of posts
     @GetMapping("/listpost")
     public String listPost(@ModelAttribute Post post, Model model) {
         model.addAttribute("book", book);
         return "listpost";
     }
-//View single post
+
+//    //Map viewpost back to list post
+//    @GetMapping("/viewpost")
+//    public String viewSinglePost(@ModelAttribute Post post, Model model) {
+//        model.addAttribute("post", post);
+//        model.addAttribute("book", book);
+//        return "redirect:/listpost";
+//    }
+
+    //View single post
     @RequestMapping("/view/{id}")
     public String viewBookPost(@PathVariable("id") long id, Model model) {
         Post post = new Post();
         for (Post one : book) {
             if (one.getId() == id) {
                 post = one;
-                book.remove(one);
+//                book.remove(one);
                 break;
             }
         }
         model.addAttribute("post", post);
-//        model.addAttribute("message", "Sign our Guestbook");
         return "viewpost";
     }
-// Update Post
+    // Update Post
     @RequestMapping("/update/{id}")
     public String updateBookPost(@PathVariable("id") long id, Model model) {
         Post post = new Post();
@@ -66,7 +74,7 @@ public class HomeCtrl {
         model.addAttribute("book", book);
         return "newpost";
     }
-// Delete post
+    // Delete post
     @RequestMapping("/delete/{id}")
     public String deleteBookPost(@PathVariable("id") long id, Model model) {
 //        Post post = new Post();
